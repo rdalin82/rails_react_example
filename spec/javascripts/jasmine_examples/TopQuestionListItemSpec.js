e72874ae5430bec91049e42abb4886c5de53a089
rdalin82 <Robert.Dalin@gmail.com>
2016-10-02 15:20:55 -0500

    updated test to match usage

--- spec/javascripts/jasmine_examples/TopQuestionListItemSpec.js
+++ spec/javascripts/jasmine_examples/TopQuestionListItemSpec.js
@@ -2,13 +2,13 @@ describe("TopQuestionsListItem", function(){
   var question;
 
   beforeEach(function(){
-    question = React.createElement(TopQuestionsListItem, { question: {q: "yo?"}, answers: ["yo"] } );
+    question = React.createElement(TopQuestionsListItem, { question: {votes: 0, answers_count: 0, views: 0, title: "what is up"}, answers: ["yo"] } );
   });
   
   it ("should have a answer", function(){
     expect(question.props.answers).toEqual(["yo"]);
   })
-  it ("should have a question", function(){
-    expect(question.props.question).toEqual({q: "yo?"});
+  it ("should have a question title", function(){
+    expect(question.props.question.title).toEqual("what is up");
   })
 })
